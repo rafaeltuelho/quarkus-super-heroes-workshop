cbf22a760ae744eed417e315bb5cefb7b43fa525
Rafael T. C. Soares <rafaelcba@gmail.com>
2022-08-18 07:12:45 -0400

    Hero Service 1st version

---
+++ super-heroes/rest-heroes/src/main/java/io/quarkus/workshop/superheroes/hero/Hero.java
@@ -0,0 +1,46 @@
+package io.quarkus.workshop.superheroes.hero;
+
+import java.util.Random;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import io.smallrye.mutiny.Uni;
+
+@Entity
+public class Hero extends PanacheEntity {
+    @NotNull
+    @Size(min = 3, max = 50)
+    public String name;
+    public String otherName;
+    @NotNull
+    @Min(1)
+    public int level;
+    public String picture;
+    @Column(columnDefinition = "TEXT")
+    public String powers;
+
+    public static Uni<Hero> findRandom() {
+        Random random = new Random();
+
+        return count()
+            .map(count -> random.nextInt(count.intValue()))
+            .chain(randomHero -> findAll().page(randomHero, 1).firstResult());
+    }
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", otherName='" + otherName + '\'' +
+            ", level=" + level +
+            ", picture='" + picture + '\'' +
+            ", powers='" + powers + '\'' +
+            '}';
+    }
+}
